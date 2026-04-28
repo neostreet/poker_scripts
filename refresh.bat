20326433c271bb6e353390976e8c8ec14733d25a
neostreet <lloyd.aidan@gmail.com>
2026-04-27 22:17:54 -0400

    Update refresh.bat

--- refresh.bat
+++ refresh.bat
@@ -12,6 +12,9 @@ mysql_grab delta2
 mysql_grab delta2_summary
 mysql_grab delta2b_summary
 mysql_grab delta6
+mysql_grab delta6_0
+unfixtxt delta6_0.out
+call copyout delta6_0
 mysql_grab delta6_summary
 unfixtxt delta6_summary.out
 call copyout delta6_summary
@@ -22,29 +25,30 @@ call streak_it delta6_summaryb
 call runtot_int_it delta6_summaryb
 mysql_grab avg_delta_by_flavor10
 mysql_grab balance
-mysql_grab 2025_balance
-mysql_grab 2025_delta_b
-mysql_grab 2025_delta6_summary
-call copyout 2025_delta6_summary
-mysql_grab 2025_delta6_summaryb
-unfixtxt 2025_delta6_summaryb.out
-call copyout 2025_delta6_summaryb
-call streak_it 2025_delta6_summaryb
-call runtot_int_it 2025_delta6_summaryb
-call runtot_int_verbose_it 2025_delta6_summaryb
-call sort_deltas_it 2025_delta6_summaryb
-call sort_deltas_reverse_it 2025_delta6_summaryb
-call sort_deltas_reverse_it 2025_delta6_summaryb.verbose.runtot_int
-call sort_deltas_it 2025_delta6_summaryb.verbose.runtot_int
-mysql_grab 2025_delta6_summaryd
-unfixtxt 2025_delta6_summaryd.out
-call copyout 2025_delta6_summaryd
-mysql_grab 2025_delta6_summaryb_mtt_nlhe
-unfixtxt 2025_delta6_summaryb_mtt_nlhe.out
-call copyout 2025_delta6_summaryb_mtt_nlhe
-call streak_it 2025_delta6_summaryb_mtt_nlhe
-call runtot_int_it 2025_delta6_summaryb_mtt_nlhe
-call runtot_int_verbose_it 2025_delta6_summaryb_mtt_nlhe
+mysql_grab year_balance
+mysql_grab year_delta_b
+mysql_grab year_delta6_summary
+call copyout year_delta6_summary
+mysql_grab year_delta6_summaryb
+unfixtxt year_delta6_summaryb.out
+call copyout year_delta6_summaryb
+call streak_it year_delta6_summaryb
+call runtot_int_it year_delta6_summaryb
+call runtot_int_verbose_it year_delta6_summaryb
+call sort_deltas_it year_delta6_summaryb
+call sort_deltas_reverse_it year_delta6_summaryb
+call sort_deltas_reverse_it year_delta6_summaryb.verbose.runtot_int
+call sort_deltas_it year_delta6_summaryb.verbose.runtot_int
+mysql_grab year_delta6_summaryd
+unfixtxt year_delta6_summaryd.out
+call copyout year_delta6_summaryd
+mysql_grab year_delta6_summaryb_mtt_nlhe
+unfixtxt year_delta6_summaryb_mtt_nlhe.out
+call copyout year_delta6_summaryb_mtt_nlhe
+call streak_it year_delta6_summaryb_mtt_nlhe
+call runtot_int_it year_delta6_summaryb_mtt_nlhe
+call fixtext_it year_delta6_summaryb_mtt_nlhe.runtot_int
+call runtot_int_verbose_it year_delta6_summaryb_mtt_nlhe
 mysql_grab delta_by_year
 unfixtxt delta_by_year.out
 call copyout delta_by_year
@@ -53,10 +57,10 @@ mysql_grab delta_by_month
 unfixtxt delta_by_month.out
 call copyout delta_by_month
 call runtot_int_it delta_by_month
-mysql_grab 2025_delta_by_month
-unfixtxt 2025_delta_by_month.out
-call copyout 2025_delta_by_month
-call runtot_int_it 2025_delta_by_month
+mysql_grab year_delta_by_month
+unfixtxt year_delta_by_month.out
+call copyout year_delta_by_month
+call runtot_int_it year_delta_by_month
 mysql_grab mtt_itm_pct
 mysql_grab mtt_nlhe_itm_pct
 mysql_grab mtt_nlhe_is_itm_lite
@@ -66,12 +70,12 @@ call streak_lite_it mtt_nlhe_is_itm_lite
 call sort_deltas_it mtt_nlhe_is_itm_lite.streak_lite
 call sort_deltas_reverse_it mtt_nlhe_is_itm_lite.streak_lite
 call run_win_pct_verbose_it mtt_nlhe_is_itm_lite
-mysql_grab 2025_is_itm_summary
-unfixtxt 2025_is_itm_summary.out
-call copyout 2025_is_itm_summary
-mysql_grab 2025_is_utl_summary
-unfixtxt 2025_is_utl_summary.out
-call copyout 2025_is_utl_summary
+mysql_grab year_is_itm_summary
+unfixtxt year_is_itm_summary.out
+call copyout year_is_itm_summary
+mysql_grab year_is_utl_summary
+unfixtxt year_is_utl_summary.out
+call copyout year_is_utl_summary
 mysql_grab sessions_by_year
 mysql_grab tournaments_by_year
 mysql_grab mtt_nlhes_by_year
@@ -79,41 +83,45 @@ mysql_grab wagered_by_year
 mysql_grab mtt_nlhe_itm_pct_by_year
 mysql_grab mtt_nlhe_roi
 mysql_grab mtt_nlhe_roi_by_year
-mysql_grab 2025_mtt_nlhe_roi
+mysql_grab year_mtt_nlhe_roi
 mysql_grab mtt_detail
 mysql_grab mtt_nlhe_detail
-mysql_grab 2025_mtt_nlhe_detail0
-unfixtxt 2025_mtt_nlhe_detail0.out
-call copyout 2025_mtt_nlhe_detail0
-mysql_grab 2025_mtt_nlhe_detail
-unfixtxt 2025_mtt_nlhe_detail.out
-call copyout 2025_mtt_nlhe_detail
-mysql_grab 2025_mtt_nlhe_itm_pct
-mysql_grab 2025_mtt_nlhe_is_itm_lite
-unfixtxt 2025_mtt_nlhe_is_itm_lite.out
-call copyout 2025_mtt_nlhe_is_itm_lite
-call streak_lite_it 2025_mtt_nlhe_is_itm_lite
-call sort_deltas_it 2025_mtt_nlhe_is_itm_lite.streak_lite
-call sort_deltas_reverse_it 2025_mtt_nlhe_is_itm_lite.streak_lite
-mysql_grab 2025_mtt_nlhe_detail0
-unfixtxt 2025_mtt_nlhe_detail0.out
-call copyout 2025_mtt_nlhe_detail0
-call streak_lite2_it 2025_mtt_nlhe_detail0
-call sort_deltas_it 2025_mtt_nlhe_detail0.streak_lite2
-call sort_deltas_reverse_it 2025_mtt_nlhe_detail0.streak_lite2
-call run_win_pct_verbose_it 2025_mtt_nlhe_is_itm_lite
-mysql_grab 2025_delta_by_style_and_flavor
+mysql_grab year_mtt_nlhe_detail
+unfixtxt year_mtt_nlhe_detail.out
+call copyout year_mtt_nlhe_detail
+mysql_grab year_mtt_nlhe_itm_pct
+mysql_grab year_mtt_nlhe_is_itm_lite
+unfixtxt year_mtt_nlhe_is_itm_lite.out
+call copyout year_mtt_nlhe_is_itm_lite
+call streak_lite_it year_mtt_nlhe_is_itm_lite
+call sort_deltas_it year_mtt_nlhe_is_itm_lite.streak_lite
+call sort_deltas_reverse_it year_mtt_nlhe_is_itm_lite.streak_lite
+mysql_grab year_mtt_nlhe_detail0
+unfixtxt year_mtt_nlhe_detail0.out
+call copyout year_mtt_nlhe_detail0
+call streak_lite2_it year_mtt_nlhe_detail0
+call sort_deltas_it year_mtt_nlhe_detail0.streak_lite2
+call sort_deltas_reverse_it year_mtt_nlhe_detail0.streak_lite2
+call streak_lite2_sum_first_it year_mtt_nlhe_detail0
+call sort_deltas_it year_mtt_nlhe_detail0.sum_first.streak_lite2
+call sort_deltas_reverse_it year_mtt_nlhe_detail0.sum_first.streak_lite2
+call run_win_pct_verbose_it year_mtt_nlhe_is_itm_lite
+mysql_grab year_delta_by_style_and_flavor
 mysql_grab mtt_nlhe_percentile2
 unfixtxt mtt_nlhe_percentile2.out
 call copyout mtt_nlhe_percentile2
 call sort_deltas_reverse_float_it mtt_nlhe_percentile2
-mysql_grab 2025_mtt_nlhe_percentile2
-unfixtxt 2025_mtt_nlhe_percentile2.out
-call copyout 2025_mtt_nlhe_percentile2
-call sort_deltas_reverse_float_it 2025_mtt_nlhe_percentile2
+mysql_grab year_mtt_nlhe_percentile2
+unfixtxt year_mtt_nlhe_percentile2.out
+call copyout year_mtt_nlhe_percentile2
+call sort_deltas_reverse_float_it year_mtt_nlhe_percentile2
 call gen_starting_ending
+call gen_starting_ending0
 call gen_ending_amount0
-mysql_grab 2025_delta_mtt_nlhe
-unfixtxt 2025_delta_mtt_nlhe.out
-call copyout 2025_delta_mtt_nlhe
-call runtot_int_it 2025_delta_mtt_nlhe
+mysql_grab year_delta_mtt_nlhe
+unfixtxt year_delta_mtt_nlhe.out
+call copyout year_delta_mtt_nlhe
+call runtot_int_it year_delta_mtt_nlhe
+mysql_grab year_mtt_nlhe_split
+call above_water_count_verbose_it year_delta6_summaryb
+call above_water_count_verbose_not_it year_delta6_summaryb
